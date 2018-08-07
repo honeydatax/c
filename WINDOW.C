db22bd52fdca7c63efcf72562b367c577c2a30e2
honey data <16432726+honeydatax@users.noreply.github.com>
2018-08-07 19:00:50 +0100

    conio.h window function

---
+++ WINDOW.C
@@ -0,0 +1,23 @@
+
+#include <conio.h>
+
+int main(void)
+{
+
+textcolor(1);
+textbackground(4);
+
+clrscr();
+
+textcolor(4);
+textbackground(1);
+
+window(10,10,40,11);
+clrscr();
+
+cprintf("im love marina.");
+
+
+   return 0;
+}
+
